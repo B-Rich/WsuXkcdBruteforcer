dfc08666b0e19818e20c3de67446e088c80857ce
Alex Schuldberg <Alex@Macintosh-2.local>
2013-04-01 16:55:40 -0700

    preformance improvements

--- bruteforce.sh
+++ bruteforce.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 for i in $(cat wordlist.txt)
 do
-	./brute $i >> results.txt
+	#./brute $i >> results.txt
 	hash=$(./brute $i)
 	j=0
 	for word in $hash
 	do
 		if [ "$j" = "2" ]; then
 			echo $word
-			if [ "$word" -lt "416" ];then
+			if [ "$word" -lt "415" ];then
 				echo $hash >> successes.txt
+				printf '/7' #play a sound
 			fi
 		fi
 		let "j += 1"
